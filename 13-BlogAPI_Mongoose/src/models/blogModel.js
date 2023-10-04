5dcfdd457338bff18aab7816a91014f971a786ab
sametdemirezen <sametdemirezen16@gmail.com>
2023-10-04 21:02:37 +0200

    blogModel

--- 13-BlogAPI_Mongoose/src/models/blogModel.js
+++ 13-BlogAPI_Mongoose/src/models/blogModel.js
@@ -4,7 +4,7 @@
 ------------------------------------------------------- */
 const mongoose = require('mongoose')
 
-const nameSchema = new  mongoose.Schema({
+/* const nameSchema = new  mongoose.Schema({
     //_id: AUTO CREATED
     //fieldName: String //short usage
     fieldName :{
@@ -24,5 +24,31 @@ const nameSchema = new  mongoose.Schema({
 }, {
     collection: 'collectionName',
     timestamps: true,
-    
+
+}) */
+
+const blogPostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    content: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    published:{
+        type: Boolean,
+        default: true
+    }
+
+},{
+    collection: 'blogPosts', timestamps: true
 })
+
+/* const BlogPostModel = mongoose.model('BlogPost', blogPostSchema) */
+module.exports = {
+    /* blogCategory: */
+    BlogPost: mongoose.model('BlogPost', blogPostSchema)
+}
