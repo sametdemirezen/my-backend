f789d65fc3c0cb24beb48673461c141cc8d08c70
sametdemirezen <sametdemirezen16@gmail.com>
2023-10-04 20:01:11 +0200

    db connection

--- 13-BlogAPI_Mongoose/src/dbConnection.js
+++ 13-BlogAPI_Mongoose/src/dbConnection.js
@@ -2,3 +2,8 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB || 'mongodb://localhost:27017')
+    .then(() => console.log('db connected'))
+    .catch(() => console.log('db not connected', err))
